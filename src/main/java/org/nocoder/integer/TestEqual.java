283645b93b4eb0cd495ba108858e54126787efc9
yangjinlong <yangjinlong@tzpt.com>
2018-09-11 13:08:16 +0800

    integer equal

---
+++ src/main/java/org/nocoder/integer/TestEqual.java
@@ -0,0 +1,14 @@
+package org.nocoder.integer;
+
+/**
+ * 包装类型间的相等判断应该用equals，而不是'=='
+ */
+public class TestEqual {
+    public static void main(String[] args) {
+        Integer x = new Integer(8);
+        Integer y = new Integer(8);
+        // 包装类型间的相等判断应该用equals，而不是'=='
+        System.out.println("x==y: " + (x == y));
+        System.out.println("x.equals(y): " + (x.equals(y)));
+    }
+}
