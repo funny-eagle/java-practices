199e92600bc14a32f275ebc833df1bbb36ba72b2
yangjinlong <yangjinlong@tzpt.com>
2018-10-30 10:57:20 +0800

    exception

---
+++ src/main/java/org/nocoder/exception/ExceptionTest.java
@@ -0,0 +1,18 @@
+package org.nocoder.exception;
+
+public class ExceptionTest {
+
+    public static void func1() throws Exception{
+        Object o = null;
+        o.equals("");
+    }
+
+
+    public static void main(String[] args){
+        try {
+            ExceptionTest.func1();
+        } catch (Exception e) {
+            System.out.println("invoker: " + e);
+        }
+    }
+}
---
+++ src/main/java/org/nocoder/exception/MyException.java
@@ -0,0 +1,8 @@
+package org.nocoder.exception;
+
+public class MyException extends Exception {
+
+    public MyException(String message) {
+        System.out.println("MyException: " + message);
+    }
+}
