276de6844608369303086b9e2a3181ea504e49dd
yangjinlong <yangjinlong@tzpt.com>
2019-01-17 17:16:46 +0800

    test concurrent hash map

--- src/main/java/org/nocoder/map/TestConcurrentHashMap.java
+++ src/main/java/org/nocoder/map/TestConcurrentHashMap.java
@@ -1,4 +1,13 @@
 package org.nocoder.map;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class TestConcurrentHashMap {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new ConcurrentHashMap<>();
+        map.put("a", 1);
+        map.put("b", 2);
+        System.out.println(map);
+    }
 }
