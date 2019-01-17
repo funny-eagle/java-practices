90f3c61bad7acb06e331dd73df60ee769946834d
yangjinlong <yangjinlong@tzpt.com>
2019-01-17 17:10:28 +0800

    统计random生成随机数字出现的次数

---
+++ src/main/java/org/nocoder/map/Statistics.java
@@ -0,0 +1,23 @@
+package org.nocoder.map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * Random 生成随机数，记录每个数字生成的次数
+ *
+ * @author yangjinlong
+ */
+public class Statistics {
+    public static void main(String[] args) {
+        Map<Integer, Integer> countingMap = new HashMap<>();
+        Random random = new Random();
+        for (int i = 0; i < 10000; i++) {
+            int key = random.nextInt(10);
+            Integer value = countingMap.get(key);
+            countingMap.put(key, value == null ? 1 : value + 1);
+        }
+        System.out.println(countingMap);
+    }
+}
