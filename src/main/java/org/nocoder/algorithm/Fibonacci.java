021a34427ab81552905b78e174e84ed0165cbd38
yangjinlong <yangjinlong86@gmail.com>
2019-05-17 15:33:30 +0800

    add fibonacci

---
+++ src/main/java/org/nocoder/algorithm/Fibonacci.java
@@ -0,0 +1,26 @@
+package org.nocoder.algorithm;
+
+/**
+ * 要求输入一个整数n，请输出斐波那契数列的第n项（从0开始，第0项为0）。
+ * 斐波那契数列:数列的后一项等于前两项的和
+ * 0, 1, 1, 2, 3, 5, 8
+ *
+ * @author jason
+ * @date 2019/5/17.
+ */
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println(fibonacci(6));
+    }
+
+    public static int fibonacci(int n) {
+        if(n == 0){return 0;}
+        int a = 0, b = 1, c = 1;
+        for (int i = 3; i <= n; i++) {
+            a = b;
+            b = c;
+            c = a + b;
+        }
+        return c;
+    }
+}
