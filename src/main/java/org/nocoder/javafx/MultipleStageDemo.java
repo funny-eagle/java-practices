a172e585c7ceb83539beb779b659e703086e4d7b
yangjinlong <yangjinlong86@gmail.com>
2021-12-27 22:13:18 +0800

    multiple stage demo

---
+++ src/main/java/org/nocoder/javafx/MultipleStageDemo.java
@@ -0,0 +1,21 @@
+package org.nocoder.javafx;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MultipleStageDemo extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Button button = new Button("OK");
+        stage.setScene(new Scene(button, 200, 200));
+        stage.setTitle("Button");
+        stage.show();
+
+        Stage stage1 = new Stage();
+        stage1.setTitle("The second stage");
+        stage1.setScene(new Scene(new Button("new Button"), 100, 100));
+        stage1.show();
+    }
+}
