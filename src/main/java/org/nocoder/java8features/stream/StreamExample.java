83833e58160e35729cbc765d7ff3ca3bf52e4812
yangjl <Nocoder8661>
2022-05-28 01:15:48 +0800

    java8 feature stream example

--- src/main/java/org/nocoder/java8features/stream/StreamExample.java
+++ src/main/java/org/nocoder/java8features/stream/StreamExample.java
@@ -11,14 +11,21 @@ public class StreamExample {
             list.add(i);
         }
 
+        // sequential stream
         Stream<Integer> sequentialStream = list.stream();
         Stream<Integer> highNumsSeq = sequentialStream.filter( s -> s > 90);
         highNumsSeq.forEach(s -> System.out.println("High Nums sequential = "+s));
 
-        // parallel 不按顺序处理值，适合数据量较大的集合
+        // parallel stream 不按顺序处理值，适合数据量较大的集合
         Stream<Integer> parallelStream = list.parallelStream();
         Stream<Integer> highNums = parallelStream.filter(p -> p > 90);
         highNums.forEach(s -> System.out.println("High Nums parallel=" + s));
 
+        System.out.println("sum Stream:" + sumStream(list));
+
+    }
+
+    private static int sumStream(List<Integer> list){
+        return list.stream().filter(n -> n > 10).mapToInt(i -> i).sum();
     }
 }
