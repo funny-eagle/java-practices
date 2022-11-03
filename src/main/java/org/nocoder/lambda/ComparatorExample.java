cc424b70697c92f1ee9609daeb3e20959c34cdbf
yangjl <yangjinlong86@gmail.com>
2022-11-03 21:10:44 +0800

    update java practice

--- src/main/java/org/nocoder/lambda/ComparatorExample.java
+++ src/main/java/org/nocoder/lambda/ComparatorExample.java
@@ -2,6 +2,7 @@ package org.nocoder.lambda;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -15,14 +16,16 @@ public class ComparatorExample {
     public static void main(String[] args) {
         List<Product> list = new ArrayList<>();
         list.add(new Product(1,"HP Laptop",25000f));
-        list.add(new Product(3,"Keyboard",300f));
-        list.add(new Product(2,"Dell Mouse",150f));
+        list.add(new Product(2,"Keyboard",300f));
+        list.add(new Product(3,"Dell Mouse",150f));
 
         System.out.println("Sorting on the basis of name...");
 
         Collections.sort(list, (p1, p2) -> {
             return p1.name.compareTo(p2.name);
         });
+
+        Collections.sort(list, Comparator.comparing(p -> p.price));
 
         for (Product p : list){
             System.out.println(p.id + " " + p.name + " " + p.price);
