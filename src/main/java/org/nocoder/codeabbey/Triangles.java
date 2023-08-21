55241c1af41bbc73a7d04e6af262506eea8fb547
yangjinlong <yangjinlong86@gmail.com>
2023-08-21 17:26:58 +0800

    验证三角形

---
+++ src/main/java/org/nocoder/codeabbey/Triangles.java
@@ -0,0 +1,29 @@
+package org.nocoder.codeabbey;
+
+import java.util.Scanner;
+
+/**
+ * https://www.codeabbey.com/index/task_view/triangles--zh
+ * @author yangjl
+ * @date 2023/8/21 17:18
+ */
+public class Triangles {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int n = input.nextInt();
+        for (int i = 0; i < n; i++) {
+            int a = input.nextInt();
+            int b = input.nextInt();
+            int c = input.nextInt();
+
+            if((a+b)>c && (a+c)>b && (b+c)>a){
+                sb.append("1");
+            }else{
+                sb.append("0");
+            }
+            sb.append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+}
