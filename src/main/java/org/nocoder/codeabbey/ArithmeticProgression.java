a1299a8151005516707e3b4f6123d387e931227f
yangjinlong <yangjinlong86@gmail.com>
2023-08-21 17:13:24 +0800

    等差数列，求a(n)，求和

---
+++ src/main/java/org/nocoder/codeabbey/ArithmeticProgression.java
@@ -0,0 +1,24 @@
+package org.nocoder.codeabbey;
+
+import java.util.Scanner;
+
+/**
+ * https://www.codeabbey.com/index/task_view/arithmetic-progression--zh
+ * @author yangjl
+ * @date 2023/8/21 16:57
+ */
+public class ArithmeticProgression {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int n = input.nextInt();
+        for (int i = 0; i < n; i++) {
+            int A = input.nextInt();
+            int B = input.nextInt();
+            int N = input.nextInt();
+            int An = A + (N-1) * B;
+            sb.append(N*(A+An)/2).append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+}
